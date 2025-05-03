618afd29ed1ac0c79edde21ec5cb5c7a3e2e7523
alejandrodenova <adenovaf@alumnes.santjosepobrer.es>
2025-05-03 22:55:48 +0200

    Añadir Flask 1

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/agua')
 def agua():
     return render_template('agua.html')
 
+
 @app.route('/aire')
 def aire():
     return render_template('aire.html')
+
 
 @app.route('/fuego')
 def fuego():
     return render_template('fuego.html')
 
+
 @app.route('/tierra')
 def tierra():
     return render_template('tierra.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
